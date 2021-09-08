05803e06b5940344668d2d516e27b7157c2dd39c
rodolfo.orlandini <rodolfo.orlandini@latourrette-consulting.com>
2021-09-07 21:53:43 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ResxToJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listFiles = Directory.GetFiles(AppContext.BaseDirectory, "*.resx", SearchOption.AllDirectories).ToList();
+
+            foreach (var file in listFiles)
+            {
+                try
+                {
+                    string xmlText = File.ReadAllText(file);
+
+                    var jsonText = "{" + Environment.NewLine;
+
+                    var listData = XElement.Parse(xmlText)
+                                           .Elements("data")
+                                           .ToList();
+
+                    listData.ForEach(delegate (XElement data)
+                    {
+                        var name = data.Attribute("name").Value;
+                        var valor = data.Element("value").Value
+                                     .Trim()
+                                     .Replace('"', '\'');
+
+                        jsonText += $"\t\"{name}\": \"{valor}\",{Environment.NewLine}";
+                    });
+
+                    jsonText += "}";
+
+                    File.WriteAllText(file.Replace(".resx", ".json"), jsonText);
+                }
+                catch { continue; }
+            }
+        }
+    }
+}
